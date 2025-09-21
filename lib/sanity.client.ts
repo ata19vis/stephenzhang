0b4ffe20295e947b985a40d69913b79c388f3104
ata19vis <stephen.x.zhang@gmail.com>
2025-09-20 19:58:47 -0500

    Update sanity.client.ts

--- lib/sanity.client.ts
+++ lib/sanity.client.ts
@@ -4,6 +4,6 @@ export const sanityClient = createClient({
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET || "production",
   apiVersion: process.env.SANITY_API_VERSION || "2025-01-01",
-  useCdn: true, // fast, cached reads
-  token: process.env.SANITY_READ_TOKEN, // not used on client; used on server if needed
+  useCdn: true,
+  token: process.env.SANITY_READ_TOKEN, // used for server-side if needed
 });
