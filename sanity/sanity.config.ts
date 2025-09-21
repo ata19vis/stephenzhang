057e53ee976a61f34442e5af43df4ec7483a7b8e
ata19vis <stephen.x.zhang@gmail.com>
2025-09-20 19:41:59 -0500

    Create sanity.config.ts

---
+++ sanity/sanity.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+import home from "./schemas/home";
+import about from "./schemas/about";
+
+export default defineConfig({
+  name: "default",
+  title: "Stephen Site CMS",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || process.env.SANITY_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || process.env.SANITY_DATASET || "production",
+  basePath: "/studio",
+  plugins: [deskTool()],
+  schema: {
+    types: [home, about],
+  },
+});
