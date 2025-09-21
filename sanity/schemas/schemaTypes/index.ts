57ef3e8cbb67299e0b2593c864cb8067112a6e94
ata19vis <stephen.x.zhang@gmail.com>
2025-09-21 00:58:19 -0500

    Update index.ts

--- sanity/schemas/schemaTypes/index.ts
+++ sanity/schemas/schemaTypes/index.ts
@@ -1,4 +1,16 @@
-import {aboutType} from './aboutType'
-import {homeType} from './homeType'
+import {defineField, defineType} from 'sanity'
 
-export const schemaTypes = [aboutType, homeType]
+const aboutType = defineType({
+  name: 'about',
+  title: 'About Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'shortBio',
+      title: 'Short Bio',
+      type: 'text',
+    }),
+  ],
+})
+
+export const schemaTypes = [aboutType]
