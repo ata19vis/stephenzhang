2118b00db9f84c8785b2854fd3224ff745ad84be
ata19vis <stephen.x.zhang@gmail.com>
2025-09-21 01:11:01 -0500

    Update index.ts

--- sanity/schemas/index.ts
+++ sanity/schemas/index.ts
@@ -1,1 +1,21 @@
-export default {};
+import {defineField, defineType} from 'sanity'
+
+const aboutType = defineType({
+  name: 'about',
+  title: 'About Page',
+  type: 'document',
+  fields: [
+    defineField({
+      name: 'shortBio',
+      title: 'Biography',
+      type: 'text',
+    }),
+    defineField({
+      name: 'cvUrl',
+      title: 'CV Link',
+      type: 'url',
+    }),
+  ],
+})
+
+export const schemaTypes = [aboutType]
