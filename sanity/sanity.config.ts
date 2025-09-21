eeecbf8b31d2513420d3ff55f9f49993f58ce4d0
ata19vis <stephen.x.zhang@gmail.com>
2025-09-21 01:10:43 -0500

    Update sanity.config.ts

--- sanity/sanity.config.ts
+++ sanity/sanity.config.ts
@@ -1,16 +1,15 @@
 import {defineConfig} from 'sanity'
 import {structureTool} from 'sanity/structure'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemaTypes'
 
 export default defineConfig({
   name: 'default',
   title: 'Stephen Site CMS',
   
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || 'production',
 
-  plugins: [structureTool(), visionTool()],
+  plugins: [structureTool()],
 
   schema: {
     types: schemaTypes,
